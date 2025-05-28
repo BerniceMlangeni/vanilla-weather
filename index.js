945c3fb7fb1eee0f52ebe96f71532b398c049451
Bernice Mlangeni <bernicemlangeni@gmail.com>
2025-05-28 09:13:16 +0200

    finished with javascript

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+
+
+     iconElement.innerHTML= ` <img src="${response.data.condition.icon_url}" class="weather-app-icon">`;
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+    windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+    timeElement.innerHTML =formatdate(date);
     temperatureElement.innerHTML = temperature;
+}
+
+function formatdate(date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -24,3 +49,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Tzaneen");
